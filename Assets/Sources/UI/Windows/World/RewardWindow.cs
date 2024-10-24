c212bbeb2a8d197542074d26c35c73992d8128e8
Virvon <dan130205@gmail.com>
2024-10-24 18:28:46 +0300

    Refactoring RewardWindow

--- Assets/Sources/UI/Windows/World/RewardWindow.cs
+++ Assets/Sources/UI/Windows/World/RewardWindow.cs
@@ -1,10 +1,30 @@
-﻿using Assets.Sources.UI.Windows.World.Panels.Reward;
+﻿using Assets.Sources.Gameplay.World.StateMachine;
+using Assets.Sources.Gameplay.World.StateMachine.States;
+using Assets.Sources.UI.Windows.World.Panels.Reward;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using Zenject;
 
 namespace Assets.Sources.UI.Windows.World
 {
     public class RewardWindow : ScreenSpaceCameraWindow
     {
-        
+        [SerializeField] private RewardsList _rewardsList;
+
+        private WorldStateMachine _worldStateMachine;
+
+        [Inject]
+        private void Construct(WorldStateMachine worldStateMachine)
+        {
+            _worldStateMachine = worldStateMachine;
+
+            _rewardsList.RewardChoosed += OnRewardChoosed;
+        }
+
+        private void OnDisable() =>
+            _rewardsList.RewardChoosed -= OnRewardChoosed;
+
+        private void OnRewardChoosed() =>
+            _worldStateMachine.Enter<WorldChangingState>().Forget();
     }
 }
